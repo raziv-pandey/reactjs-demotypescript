8d0243926140e30c203201f074c0cabd4e69d538
Rajiv Pandey <rajiv.pandey@calsoftinc.com>
2024-01-10 11:53:44 +0530

    type module in package json

--- src/app/src/auth/services/user.service.tsx
+++ src/app/src/auth/services/user.service.tsx
@@ -1,5 +1,5 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-
+//Server/jsonserver
 export const authApi = createApi({
   reducerPath: "authApi",
   baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:3006" }),
